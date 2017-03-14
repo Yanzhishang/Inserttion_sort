8757e57aa29ebd48b3e4828c054dd7c05a83869e
Yanzhishang <1176377464@qq.com>
2017-03-14 09:50:06 +0800

    Update Ins_sort.py

--- Ins_sort.py
+++ Ins_sort.py
@@ -14,6 +14,7 @@ def random_list(start, stop, length):
 print("随机生成的数组为：")
 print(random_list(start,stop,length))
 print("*"*30)
+#插入排序
 for i in range(1, len(list)): #从第二个开始
     key = list[i]
     j = i - 1
@@ -25,3 +26,11 @@ for i in range(1, len(list)): #从第二个开始
 
 print("排序后的数组为：")
 print(list)
+
+
+'''
+时间复杂度：即时间性能，高效率的排序算法应该是具有尽可能少的关键字比较次数和记录的移动次数
+空间复杂度：主要是执行算法所需要的辅助空间，越少越好。
+算法复杂性。主要是指代码的复杂性。
+插入排序的时间复杂度为 O（n^2）；空间复杂度：O（1）
+'''
